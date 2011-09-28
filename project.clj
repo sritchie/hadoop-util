13f968124abdd96fa50f250ce713211af5ce63c1
Sam Ritchie <sritchie09@gmail.com>
2011-09-28 12:17:55 -0700

    bumped version to new snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hadoop-util "0.2.1"
+(defproject hadoop-util "0.2.2-SNAPSHOT"
   :description "Hadoop utilities that we've found useful."
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
