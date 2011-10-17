cf7bd921eba2c10542caa1277f458e446f884585
Sam Ritchie <sritchie09@gmail.com>
2011-10-17 12:15:10 -0700

    bumped midje and lein versions in dev deps.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [lein-marginalia "0.6.0"]
-                     [lein-midje "1.0.3"]
-                     [midje "1.2.0"]])
+                     [lein-midje "1.0.4"]
+                     [midje "1.3-alpha4"]])
