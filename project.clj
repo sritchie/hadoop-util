a13342ff739cd6508408af1e87f782e65d6894cd
Sam Ritchie <sritchie09@gmail.com>
2011-10-20 12:18:15 -0700

    bumped marginalia version.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [lein-marginalia "0.6.0"]
+                     [lein-marginalia "0.6.1"]
                      [lein-midje "1.0.4"]
                      [midje "1.3-alpha4"]])
