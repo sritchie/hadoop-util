bd588cf3b7b639a0de31d8c499c26f9c18a273f1
Sam Ritchie <sritchie09@gmail.com>
2011-12-20 13:29:25 -0500

    Added throttled recursive copy.

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
-(defproject hadoop-util "0.2.4-SNAPSHOT"
+(defproject hadoop-util "0.2.5"
   :description "Hadoop utilities that we've found useful."
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
-                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [lein-marginalia "0.6.1"]
-                     [lein-midje "1.0.4"]
-                     [midje "1.3-alpha4"]])
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                     [midje "1.3.0"]])
